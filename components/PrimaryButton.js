49b958fd11a283f3dd6b93cb06fb4e728132ee3d
04Marsha <161226959+04Marsha@users.noreply.github.com>
2025-09-18 18:38:44 +0530

    Game screen

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -1,10 +1,6 @@
 import { StyleSheet, View, Text, Pressable } from "react-native";
 
-function PrimaryButton({ children }) {
-  function onPressHandler() {
-    console.log("Pressed");
-  }
-
+function PrimaryButton({ children, onPress }) {
   return (
     <View style={styles.buttonOuterContainer}>
       <Pressable
@@ -13,7 +9,7 @@ function PrimaryButton({ children }) {
             ? [styles.buttonInnerContainer, styles.pressed]
             : styles.buttonInnerContainer
         }
-        onPress={onPressHandler}
+        onPress={onPress}
         android_ripple={{ color: "#640233" }}
       >
         <Text style={styles.buttonText}>{children}</Text>
