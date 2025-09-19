af665d990cb8439dbbd56659f5da43dfbc4d6a98
04Marsha <161226959+04Marsha@users.noreply.github.com>
2025-09-19 18:27:01 +0530

    game screen

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,24 +1,58 @@
+import { useState } from "react";
 import { StyleSheet, View, Text } from "react-native";
-import Title from "../components/Title";
 
-function GameScreen() {
-    return <View style={styles.screen}>
-        <Title> Opponent's Guess </Title>
-        {/* GUESS */}
+import Title from "../components/ui/Title";
+import NumberContainer from "../components/game/NumberContainer";
+import PrimaryButton from "../components/ui/PrimaryButton";
+
+function generateRandomBetween(min, max, exclude) {
+  const rndNum = Math.floor(Math.random() * (max - min)) + min;
+
+  if (rndNum === exclude) {
+    return generateRandomBetween(min, max, exclude);
+  } else {
+    return rndNum;
+  }
+}
+
+let minBoundary = 1;
+let maxBoundary = 100;
+
+function GameScreen({ userNumber }) {
+  const initialGuess = generateRandomBetween(minBoundary, maxBoundary, userNumber);
+  const [currentGuess, setCurrentGuess] = useState(initialGuess);
+
+  function nextGuessHandler(direction) {
+    if (direction === 'lower') {
+        maxBoundary = currentGuess;
+    } else {
+        minBoundary = currentGuess + 1;
+    }
+    const newRndNumber = generateRandomBetween(minBoundary, maxBoundary, currentGuess);
+    setCurrentGuess(newRndNumber)
+  }
+
+  return (
+    <View style={styles.screen}>
+      <Title> Opponent's Guess </Title>
+      <NumberContainer> {currentGuess} </NumberContainer>
+      <View>
+        <Text>Higher or lower?</Text>
         <View>
-            <Text>Higher or lower?</Text>
-            {/* +
-            - */}
+          <PrimaryButton onPress={nextGuessHandler.bind(this, 'lower')} >-</PrimaryButton>
+          <PrimaryButton onPress={nextGuessHandler.bind(this, 'greater')} >+</PrimaryButton>
         </View>
-        {/* <View>LOG ROUNDS</View> */}
+      </View>
+      {/* <View>LOG ROUNDS</View> */}
     </View>
+  );
 }
 
 export default GameScreen;
 
 const styles = StyleSheet.create({
-    screen: {
-        flex: 1,
-        padding: 24,
-    },
-})
+  screen: {
+    flex: 1,
+    padding: 24,
+  },
+});
